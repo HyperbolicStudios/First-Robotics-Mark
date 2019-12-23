7665d60417e9b970262331802cfe1170799401c6
HyperbolicStudios <marke9135@sd61learn.ca>
2019-12-23 13:52:58 -0800

    Programming Update
    Code as of December 22, 2019. Includes:
     - An updated driving program that works (not sure about pivot though)
    - The beginning of the autonomous program that drives forward until a certain distance is sensed by the colour sensor.

--- Auto.java
+++ Auto.java
@@ -1,85 +1,72 @@
+// simple autonomous program that drives bot forward 2 seconds then ends.
+
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.hardware.Gyroscope;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.Gamepad;
-import org.firstinspires.ftc.robotcontroller.external.samples.SensorREVColorDistance;
+
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-@TeleOp //FIX
+// below is the Annotation that registers this OpMode with the FtcRobotController app.
+// @Autonomous classifies the OpMode as autonomous, name is the OpMode title and the
+// optional group places the OpMode into the Exercises group.
+// uncomment the @Disable annotation to remove the OpMode from the OpMode list.
 
-public class MyFIRSTJavaOpMode extends LinearOpMode {
-    private DcMotor motorLeft;
+@Autonomous(name="Drive Forward", group="Exercises")
+//@Disabled
+public class Auto extends LinearOpMode
+{
+    private DcMotor motorLeft; //Not relevant
     private DcMotor motorRight;
     private DcMotor armA1;
     private DcMotor motorStrafe;
-    private Servo chassis1;
-    private Servo chassis2;
-    private Servo Grab;
-    private DistanceSensor sensorCRFront;
+    private Servo LeftFound;
+    private Servo RightFound;
+    private Servo grab;
+    private Servo grab2;
+    private Servo grabPivot;
+    private DistanceSensor front;
 
-
+    // called when init button is  pressed.
 
     @Override
-    public void runOpMode() {
-        motorLeft = hardwareMap.get(DcMotor.class, "left");
+    public void runOpMode() throws InterruptedException
+    {
+        motorLeft = hardwareMap.get(DcMotor.class, "left");     //Create devices - don't worry about this
         motorRight = hardwareMap.get(DcMotor.class, "right");
         armA1 = hardwareMap.get(DcMotor.class, "ArmA1");
         motorStrafe = hardwareMap.get(DcMotor.class, "strafe");
-        Grab = hardwareMap.get(Servo.class, "Grab");
-        chassis1 = hardwareMap.get(Servo.class, "chassis1");
-        chassis2 = hardwareMap.get(Servo.class, "chassis2");
-        sensorCRFront = hardwareMap.get(DistanceSensor.class, "sensorColorRangeFront");
+        grab = hardwareMap.get(Servo.class, "Grab");
+        grab2 = hardwareMap.get(Servo.class, "Grab2");
+        grabPivot = hardwareMap.get(Servo.class, "grabPivot");
+        //LeftFound = hardwareMap.get(Servo.class, "LeftFound");
+        //RightFound = hardwareMap.get(Servo.class, "LeftFound");
+        front = hardwareMap.get(DistanceSensor.class, "front");
+        //sensorCRBottom = hardwareMap.get(DistanceSensor.class, "sensorColorRangeFront");
+        //sensorCRSide = hardwareMap.get(DistanceSensor.class, "sensorColorRangeFront");
         telemetry.addData("Status", "Initialized");
         telemetry.update();         // Wait for the game to start (driver presses PLAY)
+
+
+        telemetry.addData("Mode", "waiting");
+        telemetry.addData("Front (cm)", front.getDistance(DistanceUnit.CM));
+        telemetry.update();
+
+        // wait for start button.
+
         waitForStart();
-        // run until the end of the match (driver presses STOP)
-        double powerLeft = 0;
-        double powerRight = 0;
-        double armPower = 0;
-        double strafePower = 0;
-        double x;
-        double y;
-        double h;
-        while (opModeIsActive()) {
+        while(opModeIsActive()) {
+            if (front.getDistance(DistanceUnit.CM) > 15 && front.getDistance(DistanceUnit.CM) < 2) {
+                motorStrafe.setPower(1);
+            } else {
+                motorStrafe.setPower(0);
+            }
 
-            //Drive forward
-
-            //Wait for Button to be pressed
-
-            //Drop Servos
-
-            //Drive backward
-
-            //Wait for rear button to be pressed
-
-            //Strafe Sideways
-
-
-
-
-
-//TRANSMIT POWER INFO TO HUB
-            motorLeft.setPower(powerLeft); //Drive
-            motorRight.setPower(powerRight);
-            motorStrafe.setPower(strafePower);
-            armA1.setPower(armPower); //Stage 1
-
-
-            telemetry.addData("Left Drive Motor Power", motorLeft.getPower());
-            telemetry.addData("Right Drive Motor Power", motorRight.getPower());
-            telemetry.addData("Strafe Power", motorStrafe.getPower());
-            telemetry.addData("Arm Power", armA1.getPower());
-
-            telemetry.addData("Servo Position", Grab.getPosition());
-            telemetry.addData("Distance (cm)", sensorCRFront.getDistance(DistanceUnit.CM));
-            telemetry.addData("Status", "Running");
+            telemetry.addData("Mode", "running");
+            telemetry.addData("Front (cm)", front.getDistance(DistanceUnit.CM));
             telemetry.update();
         }
     }
--- SkyStone-master-flex/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
+++ SkyStone-master-flex/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
@@ -19,10 +19,13 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
     private DcMotor motorRight;
     private DcMotor armA1;
     private DcMotor motorStrafe;
-    private Servo chassis1;
-    private Servo chassis2;
-    private Servo Grab;
-    private DistanceSensor sensorCRFront;
+    private Servo LeftFound;
+    private Servo RightFound;
+    private Servo grab;
+    private Servo grab2;
+    private Servo grabPivot;
+    private DistanceSensor front;
+
 
 
 
@@ -32,10 +35,14 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         motorRight = hardwareMap.get(DcMotor.class, "right");
         armA1 = hardwareMap.get(DcMotor.class, "ArmA1");
         motorStrafe = hardwareMap.get(DcMotor.class, "strafe");
-        Grab = hardwareMap.get(Servo.class, "Grab");
-        chassis1 = hardwareMap.get(Servo.class, "chassis1");
-        chassis2 = hardwareMap.get(Servo.class, "chassis2");
-        sensorCRFront = hardwareMap.get(DistanceSensor.class, "sensorColorRangeFront");
+        grab = hardwareMap.get(Servo.class, "Grab");
+        grab2 = hardwareMap.get(Servo.class, "Grab2");
+        grabPivot = hardwareMap.get(Servo.class, "grabPivot");
+        //LeftFound = hardwareMap.get(Servo.class, "LeftFound");
+        //RightFound = hardwareMap.get(Servo.class, "LeftFound");
+        front = hardwareMap.get(DistanceSensor.class, "front");
+        //sensorCRBottom = hardwareMap.get(DistanceSensor.class, "sensorColorRangeFront");
+        //sensorCRSide = hardwareMap.get(DistanceSensor.class, "sensorColorRangeFront");
         telemetry.addData("Status", "Initialized");
         telemetry.update();         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -44,41 +51,34 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         double powerRight = 0;
         double armPower = 0;
         double strafePower = 0;
-        double x;
         double y;
-        double h;
+        boolean prec = false;
         while (opModeIsActive()) {
-            x = this.gamepad1.left_stick_x; //Get x value of joystick
             y = this.gamepad1.left_stick_y; //Get y value of joystick
-            h = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)); //Get distance from center ("h")
 
-            if(y < 0) { //allow reverse movement
-                h = h*-1;
+            //check for precision mode
+            if (gamepad1.right_trigger > 0) {
+                prec = true;
+            } else {
+                prec = false;
             }
 
-            powerLeft = -h; //Set initial power value
-            powerRight = h;
-
-            if (x > 0) { //If the joystick is on the right (range from 0 to 1)
-                powerRight *= x; //Multiply the right motor by x
-            } else if (x < 0) {//If the joystick is on the left (range from -1 to 0)
-                powerLeft *= -x; //Multiply the left motor by x
-            }
+            armPower = y / 2;
 
 
 //Check if the bumper is pressed; this causes a pivot. A pivot will OVERRIDE previous math done.
 
             if (gamepad1.left_bumper) {//Pivot Counterclockwise
-                powerRight = 1; //Maximum right power
-                powerLeft = 1; //Maximum left power (other direction)
+                powerLeft = 1; //Maximum right power
+                powerRight = 1; //Maximum left power (other direction)
             }
             else if (gamepad1.right_bumper) { //Clockwise direction
                 powerRight = -1; //Likewise
                 powerLeft = -1;
-            }
+            }//if else
 
 
-//Check for strafe power - this will not overide other motor powers.
+//Check for strafe power - this will not override other motor powers.
 
             if (gamepad1.dpad_left) {
                 strafePower = 1;
@@ -88,38 +88,52 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
             }
             else {
                 strafePower = 0;
-            }
-//At this point, the drive motors have been set.
+            }//if else
 
-            if(gamepad1.a){ //First Stage Arm
-                armPower = 0.4;
-            } else if (gamepad1.b) {
-                armPower = -0.4;
-            } else {
-                armPower = 0;
+            if (gamepad1.dpad_up) {
+                powerLeft = 1;
+                powerRight = -1;
             }
-
-            if(gamepad1.x) { //Chassis servos - grab foundation
-                chassis1.setPosition(90);
-                chassis1.setPosition(90);
+            else if (gamepad1.dpad_down) {
+                powerLeft = -1;
+                powerRight = 1;
             }
-            else if(gamepad1.y) {
-                chassis1.setPosition(0);
-                chassis2.setPosition(0);
+            else {
+                powerLeft = 0;
+                powerRight = 0;
             }
 
+//Reduces power if precision mode is enabled
+
+        if (prec) {
+            powerLeft /= 3;
+            powerRight /= 3;
+            armPower /= 3;
+            strafePower /= 3;
+        }
+
+          //At this point, the motors have been set
 
 
-            if(this.gamepad1.right_stick_y < 0){ //Grabber
-                Grab.setPosition(1);
-            } else if (this.gamepad1.right_stick_y > 0) {
-                Grab.setPosition(0);
-            } else {
-                Grab.setPosition(.6);
+          if(gamepad1.a) {
+                grab.setPosition(180);
+                grab2.setPosition(0);
+
+            }
+          else if (gamepad1.b) {
+                grab.setPosition(0);
+                grab2.setPosition(180);
             }
 
+          if (gamepad1.x) {
+              grabPivot.setPosition(0);
+          } else if (gamepad1.y) {
+              grabPivot.setPosition(180);
+          }
 
-
+          if (armPower == 0) {
+              armPower = -0.05;
+          }
 
 
 //TRANSMIT POWER INFO TO HUB
@@ -133,11 +147,11 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
             telemetry.addData("Right Drive Motor Power", motorRight.getPower());
             telemetry.addData("Strafe Power", motorStrafe.getPower());
             telemetry.addData("Arm Power", armA1.getPower());
+            telemetry.addData("Front Distance (cm)", front.getDistance(DistanceUnit.CM));
 
-            telemetry.addData("Servo Position", Grab.getPosition());
-            telemetry.addData("Distance (cm)", sensorCRFront.getDistance(DistanceUnit.CM));
+            //telemetry.addData("R Servo Position", RightFound.getPosition());
+            //telemetry.addData("L Servo Position: ", LeftFound.getPosition());
             telemetry.addData("Status", "Running");
             telemetry.update();
         }
-    }
-}
+    }}
